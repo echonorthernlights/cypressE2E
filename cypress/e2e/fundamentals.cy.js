25c6bb30bae8a45372340b84433b2925cd07e345
echonorthernlights <elhassanianas@gmail.com>
2023-08-20 19:54:03 +0100

    Cypress fundamentals

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,24 @@
 describe("Fundamentals test", () => {
-  it("passes", () => {
-    cy.visit("http://localhost:3000");
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("Contains correct header text", () => {
+    cy.getDataTest("home-header").contains(/Why cypress?/i);
+  });
+  it("Accordion works correctly", () => {
+    cy.contains(
+      /An all-in-one testing framework, assertion library, with mocking and stubbing/i
+    ).should("not.be.visible");
+
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+
+    cy.contains(
+      /An all-in-one testing framework, assertion library, with mocking and stubbing/i
+    ).should("be.visible");
+
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+    cy.contains(
+      /An all-in-one testing framework, assertion library, with mocking and stubbing/i
+    ).should("not.be.visible");
   });
 });
